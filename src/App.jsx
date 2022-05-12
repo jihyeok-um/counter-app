421cb3fba0a71d0a9978468db663ae51f99e1467
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-05-12 11:04:50 +0900

    chore: 폴더 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import './App.css';
-import { useState } from 'react';
+import "./css/App.css";
+import { useState } from "react";
 
-import ButtonGroup from './ButtonGroup';
-import ShowCount from './ShowCount';
-import InputRange from './InputRange';
+import ButtonGroup from "./components/ButtonGroup";
+import ShowCount from "./components/ShowCount";
+import InputRange from "./components/InputRange";
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [diff, setDiff] = useState(0);
+  const [count, setCount] = useState(0);
+  const [diff, setDiff] = useState(0);
 
-	const onIncrement = () => setCount((prevCount) => prevCount + (diff || 1));
-	const onDecrement = () => setCount((prevCount) => prevCount - (diff || 1));
-	const onReset = () => setCount(0);
+  const onIncrement = () => setCount((prevCount) => prevCount + (diff || 1));
+  const onDecrement = () => setCount((prevCount) => prevCount - (diff || 1));
+  const onReset = () => setCount(0);
 
-	const handleDiff = ({ target }) => setDiff(target.valueAsNumber);
+  const handleDiff = ({ target }) => setDiff(target.valueAsNumber);
 
-	return (
-		<div className="App">
-			<main className="App-main">
-				<ShowCount count={count} diff={diff} />
-				<InputRange handleDiff={handleDiff} diff={diff} />
-				<ButtonGroup onDecrement={onDecrement} onReset={onReset} onIncrement={onIncrement} />
-			</main>
-		</div>
-	);
+  return (
+    <div className="App">
+      <main className="App-main">
+        <ShowCount count={count} diff={diff} />
+        <InputRange handleDiff={handleDiff} diff={diff} />
+        <ButtonGroup
+          onDecrement={onDecrement}
+          onReset={onReset}
+          onIncrement={onIncrement}
+        />
+      </main>
+    </div>
+  );
 }
 
 export default App;
